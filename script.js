35ce203a069ec367f1b77f809aae5adc02b4de49
arnav miranda <arnavmiranda@gmail.com>
2025-08-21 22:28:53 +0530

    completed basic functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let SQUARE_COUNT = 16;
+let SIDE_LENGTH = window.innerHeight * 0.9 / SQUARE_COUNT;
 
 const container = document.querySelector(".container");
 formGrid(SQUARE_COUNT);
@@ -12,6 +13,9 @@ button.addEventListener("click", () => {
 });
 
 function formGrid(num) {
+    num = Math.min(num, 128);
+    SIDE_LENGTH = window.innerHeight * 0.9 / num;
+
     container.innerHTML = "";
     for (let i = 1; i <= num; i++) {
         let rowContainer = document.createElement("div");
@@ -20,6 +24,8 @@ function formGrid(num) {
             let cell = document.createElement("div");
             cell.setAttribute("class", "cell");
             cell.style.boxSizing = "border-box";
+            cell.style.width = SIDE_LENGTH + "px";
+            cell.style.height = SIDE_LENGTH + "px";
             rowContainer.appendChild(cell);
         }
         container.appendChild(rowContainer);
